adcd19b4caedd64561b2d58f9504ecfc22cd8e17
Your Name <you@example.com>
2017-03-15 21:52:24 -0400

    added bodyparser/ implemented campground post capabilites

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+
+var campgrounds = [
+      {name:"Salmon Creek", image:"https://i.ytimg.com/vi/n6RtLye_qJM/maxresdefault.jpg"},
+      {name:"Granite Hill", image:"http://access.parks.ca.gov/parkpictures//PrairieCreekCabin.jpg"},
+      {name:"Dead man's cliff", image:"http://52583a043dc5b868d166-4f8f580dda5ee644b03eed875d323fab.r95.cf2.rackcdn.com/55fb1f8aa097a/55fb1f8aa097a-poster.jpg"},
+      {name:"euakton valley", image:"http://haaslakepark.com/wp-content/uploads/2015/01/RV-by-Water2.jpg"},
+      {name:"passapuiqua Peaks", image:"http://trekohio.com/wp-content/uploads/20161002-farnsworth-metropark-riverside-campsite-dscf0838.jpg"}
+  ]
+
 
 app.listen(3000,function(){
     console.log("yelpcamp server started");
@@ -13,13 +24,18 @@ app.get("/",function(req,res){
 });
 
 app.get("/campgrounds",function(req,res){
-  var campgrounds = [
-      {name:"Salmon Creek", image:"https://i.ytimg.com/vi/n6RtLye_qJM/maxresdefault.jpg"},
-      {name:"Granite Hill", image:"http://access.parks.ca.gov/parkpictures//PrairieCreekCabin.jpg"},
-      {name:"Dead man's cliff", image:"http://52583a043dc5b868d166-4f8f580dda5ee644b03eed875d323fab.r95.cf2.rackcdn.com/55fb1f8aa097a/55fb1f8aa097a-poster.jpg"},
-      {name:"euakton valley", image:"http://haaslakepark.com/wp-content/uploads/2015/01/RV-by-Water2.jpg"},
-      {name:"passapuiqua Peaks", image:"http://trekohio.com/wp-content/uploads/20161002-farnsworth-metropark-riverside-campsite-dscf0838.jpg"}
-  ]
-
+  
   res.render("campgrounds",{campgrounds:campgrounds});
 });
+
+app.post("/campgrounds", function(req,res){
+var name = req.body.name;
+var image = req.body.image;
+var newCampground = {name:name,image:image}
+campgrounds.push(newCampground);
+res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+  res.render("new.ejs")
+})
